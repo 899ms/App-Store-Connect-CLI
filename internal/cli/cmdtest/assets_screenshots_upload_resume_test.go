b78b845c1896221a1e508a57e66ed200ef3987a4
rudrankriyam <rudrankriyam@gmail.com>
2026-09-23 05:59:25 +0530

    Stabilize sequential screenshot failure fixture

--- internal/cli/cmdtest/assets_screenshots_upload_resume_test.go
+++ internal/cli/cmdtest/assets_screenshots_upload_resume_test.go
@@ -407,6 +407,7 @@ func TestRunScreenshotsUploadFanoutPrintsPartialResultsOnLocaleFailure(t *testin
 			"--version-id", "version-1",
 			"--path", workDir,
 			"--device-type", "IPHONE_65",
+			"--concurrency", "1",
 			"--output", "json",
 		}, "1.2.3")
 		if code != cmd.ExitError {
